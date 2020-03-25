cfde15b877693d09674956c254d2e643595528a3
jtan052 <43653652+jtan052@users.noreply.github.com>
2020-03-25 07:43:30 -0700

    Enemy Waves and Health

--- WizardShot/Assets/Scripts/EnemyPathing.cs
+++ WizardShot/Assets/Scripts/EnemyPathing.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour
 {
-    [SerializeField] List<Transform> waypoints;
+    WaveConfig waveConfig;
+    List<Transform> waypoints;
+    
+    int waypointIndex = 0;
     void Start()
     {
-        
+        waypoints = waveConfig.GetWaypoints();
+        transform.position = waypoints[waypointIndex].transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Pathing();
+    }
+
+    public void SetWaveConfig(WaveConfig waveConfig)
+    {
+        this.waveConfig = waveConfig;
+    }
+
+    private void Pathing()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[waypointIndex].transform.position;
+            var movementThisFrame = waveConfig.GetMoveSpeed() * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+            
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
+ 
